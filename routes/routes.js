9fbcbc94b88988f64183244c664e4475ebb3bbbe
estebanzarate <estebanzaratedev@gmail.com>
2023-01-04 22:14:37 -0300

    added utils

--- routes/routes.js
+++ routes/routes.js
@@ -1,23 +1,25 @@
 import { Router } from 'express';
 import path from 'path';
-import { __dirname } from '../server.js';
+import { __dirname } from '../utils/utils.js';
 
 const router = Router();
 
-router.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, '/../public/index.html')));
 
-router.get('/about', (req, res) => res.sendFile(path.join(__dirname, '/public/pages/about.html')));
+router.get('/about', (req, res) =>
+	res.sendFile(path.join(__dirname, '/../public/pages/about.html'))
+);
 
 router.get('/black-market', (req, res) =>
-	res.sendFile(path.join(__dirname, '/public/pages/black-market.html'))
+	res.sendFile(path.join(__dirname, '/../public/pages/black-market.html'))
 );
 
 router.get('/gallery', (req, res) =>
-	res.sendFile(path.join(__dirname, '/public/pages/gallery.html'))
+	res.sendFile(path.join(__dirname, '/../public/pages/gallery.html'))
 );
 
 router.get('/contact', (req, res) =>
-	res.sendFile(path.join(__dirname, '/public/pages/contact.html'))
+	res.sendFile(path.join(__dirname, '/../public/pages/contact.html'))
 );
 
 export default router;
--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 import express from 'express';
+import { __dirname } from './utils/utils.js';
 import router from './routes/routes.js';
-
-import url from 'url';
-import path from 'path';
-
-const __filename = url.fileURLToPath(import.meta.url);
-export const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/../public'));
 
 app.use('/', router);
 
